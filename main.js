d8ebea93e98159607f1c7f1d90b2180808576d1c
Bruno Turmina <bruno.turmina@gmail.com>
2024-07-12 12:58:46 +0200

    new version

--- main.js
+++ main.js
@@ -1,12 +1,5 @@
-const { app, BrowserWindow, ipcMain, dialog } = require('electron/main')
+const { app, BrowserWindow, ipcMain } = require('electron/main')
 const path = require('node:path')
-
-async function handleFileOpen () {
-  const { canceled, filePaths } = await dialog.showOpenDialog()
-  if (!canceled) {
-    return filePaths[0]
-  }
-}
 
 function createWindow () {
   const mainWindow = new BrowserWindow({
@@ -14,12 +7,19 @@ function createWindow () {
       preload: path.join(__dirname, 'preload.js')
     }
   })
+
+  ipcMain.on('set-title', (event, title) => {
+    const webContents = event.sender
+    const win = BrowserWindow.fromWebContents(webContents)
+    win.setTitle(title)
+  })
+
   mainWindow.loadFile('index.html')
 }
 
 app.whenReady().then(() => {
-  ipcMain.handle('dialog:openFile', handleFileOpen)
   createWindow()
+
   app.on('activate', function () {
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
--- renderer.js
+++ renderer.js
@@ -1,7 +1,6 @@
-const btn = document.getElementById('btn')
-const filePathElement = document.getElementById('filePath')
-
-btn.addEventListener('click', async () => {
-  const filePath = await window.electronAPI.openFile()
-  filePathElement.innerText = filePath
+const setButton = document.getElementById('btn')
+const titleInput = document.getElementById('title')
+setButton.addEventListener('click', () => {
+  const title = titleInput.value
+  window.electronAPI.setTitle(title)
 })
